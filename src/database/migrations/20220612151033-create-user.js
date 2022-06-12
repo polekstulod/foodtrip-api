11255b1e6b2d850b89dd1b84c3e29d9ab4a7932f
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-06-13 00:59:01 +0800

    Add user model - Add column (user_id, user_no, password, first_name, middle_name,   last_name, email_address, phone_number, gender, user_type, status,   date_created, date_updated, created_by, updated_by)

---
+++ src/database/migrations/20220612151033-create-user.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
