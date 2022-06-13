d9f0f0c9d2c50764203acfac4c913406d7aca930
paultulod <paultulod@pm.me>
2022-06-14 02:21:25 +0800

    Update User controller - Add find single user function - Add update user function - Add delete user function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -45,10 +45,102 @@ exports.findAll = (req, res) => {
 };
 
 // * Find single User
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+	const id = req.params.id;
+
+	User.findByPk(id)
+		.then((data) => {
+			res.send({
+				error: false,
+				data: data,
+				message: [process.env.SUCCESS_RETRIEVED],
+			});
+		})
+		.catch((err) => {
+			res.status(500).send({
+				error: true,
+				data: [],
+				message:
+					err.errors.map((e) => e.message) || process.env.GENERAL_ERROR_MSG,
+			});
+		});
+};
 
 // * Update User
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+	const id = req.params.id;
+
+	if (req.body.password) {
+		req.body.password = await bcrypt.hash(
+			req.body.password,
+			parseInt(process.env.SALT_ROUNDS)
+		);
+	}
+
+	User.update(req.body, {
+		where: { user_id: id },
+	})
+		.then((result) => {
+			console.log(result);
+			if (result) {
+				// success
+				User.findByPk(id).then((data) => {
+					res.send({
+						error: false,
+						data: data,
+						message: [process.env.SUCCESS_UPDATE],
+					});
+				});
+			} else {
+				res.status(500).send({
+					error: true,
+					data: [],
+					message: ['Error in updating a record'],
+				});
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send({
+				error: true,
+				data: [],
+				message:
+					err.errors.map((e) => e.message) || process.env.GENERAL_ERROR_MSG,
+			});
+		});
+};
 
 // * Delete User
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+	const id = req.params.id;
+	const body = { status: 'Inactive' };
+
+	User.update(body, {
+		where: { user_id: id },
+	})
+		.then((result) => {
+			if (result) {
+				User.findByPk(id).then((data) => {
+					res.send({
+						error: false,
+						data: data,
+						message: [process.env.SUCCESS_UPDATE],
+					});
+				});
+			} else {
+				res.status(500).send({
+					error: true,
+					data: [],
+					message: ['Error in updating a record'],
+				});
+			}
+		})
+		.catch((err) => {
+			res.status(500).send({
+				error: true,
+				data: [],
+				message:
+					err.errors.map((e) => e.message) || process.env.GENERAL_ERROR_MSG,
+			});
+		});
+};
