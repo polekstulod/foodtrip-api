bc438623446b9d96afa2a21a465836e4b8736dec
paultulod <paultulod@pm.me>
2022-06-30 22:13:28 +0800

    Add authorization on User controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 const db = require('../models');
 const User = db.User;
 const bcrypt = require('bcrypt');
+const helper = require('../helpers/controller.helper');
 
 // * Create user
 exports.create = async (req, res) => {
+	if (!helper.checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
 	req.body.user_no = `${Math.floor(Math.random() * (9999999999 - 1 + 1) + 1)}`;
 	req.body.created_by = req.user.user_id;
 
@@ -29,6 +33,9 @@ exports.create = async (req, res) => {
 
 // * Retrieve all User
 exports.findAll = (req, res) => {
+	if (!helper.checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
 	User.findAll({ include: ['created', 'restaurant'] })
 		.then((data) => {
 			res.send({
@@ -48,6 +55,9 @@ exports.findAll = (req, res) => {
 
 // * Find single User
 exports.findOne = (req, res) => {
+	if (!helper.checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
 	const id = req.params.id;
 
 	User.findByPk(id, { include: ['created', 'restaurant'] })
@@ -69,6 +79,9 @@ exports.findOne = (req, res) => {
 
 // * Update User
 exports.update = async (req, res) => {
+	if (!helper.checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
 	const id = req.params.id;
 	req.body.updated_by = req.user.user_id;
 
@@ -107,6 +120,10 @@ exports.update = async (req, res) => {
 
 // * Delete User
 exports.delete = async (req, res) => {
+	if (!helper.checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
+
 	const id = req.params.id;
 	req.body.deleted_by = req.user.user_id;
 
