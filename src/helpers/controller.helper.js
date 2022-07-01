111159aa3b8f0aa6d4b220896b7902dfceb973ed
paultulod <paultulod@pm.me>
2022-07-01 17:46:23 +0800

    Update controller helper

--- src/helpers/controller.helper.js
+++ src/helpers/controller.helper.js
@@ -1,23 +1,48 @@
 exports.checkAuthorization = (req, res, user_type) => {
-	// Check if user_type param is null
 	if (user_type == null) {
 		res.status(500).send('`user_type` parameter is required');
 		return false;
 	}
 
-	// Check if user_type param has valid value
 	const validuser_type = ['Customer', 'Resto_Admin', 'Admin'].some((role) => role === user_type);
 
-	// Validate user_type parameter
 	if (!validuser_type) {
 		res.status(500).send('The value for `user_type` parameter is invalid');
 		return false;
 	}
 
-	// Check if user is not authorized
 	if (!(req.user != null && req.user.user_type === user_type)) {
 		res.status(401).send('Oops! You are unauthorized to view your request');
 		return false;
 	}
 	return true;
+};
+
+exports.errResponse = (res, err) => {
+	return res.status(500).send({
+		error: true,
+		message: `${err}`,
+	});
+};
+
+exports.dataResponse = (res, data, withDataMsg, nullDataMsg) => {
+	if (data.length === 0 || data == null)
+		return res.send({
+			error: false,
+			data: [],
+			message: nullDataMsg,
+		});
+
+	return res.send({
+		error: false,
+		data: data,
+		message: withDataMsg,
+	});
+};
+
+exports.emptyDataResponse = (res, message) => {
+	return res.send({
+		error: false,
+		message: message,
+	});
 };
