8390f19670ee5882afd03badd4069d0867f10a07
paultulod <paultulod@pm.me>
2022-07-04 12:04:04 +0800

    Fix restaurant file upload - Remove upload function - Add file upload from middleware

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,54 +1,5 @@
 var router = require('express').Router();
-const multer = require('multer');
-const path = require('path');
-const helpers = require('../helpers/imageHelper');
-
-const restoStorage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, path.join(__dirname, '../../public/uploads/restaurants/'));
-	},
-
-	filename: function (req, file, cb) {
-		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-	},
-});
-
-const uploadRestoImage = (req, res, next) => {
-	let restoUpload = multer({
-		storage: restoStorage,
-		fileFilter: helpers.imageFilter,
-	}).single('resto_img');
-
-	restoUpload(req, res, function (err) {
-		if (req.fileValidationError) {
-			return res.status(500).send({
-				error: true,
-				data: [],
-				message: [req.fileValidationError],
-			});
-		} else if (!req.file) {
-			return res.status(500).send({
-				error: true,
-				data: [],
-				message: ['Please select an image to upload'],
-			});
-		} else if (err instanceof multer.MulterError) {
-			return res.status(500).send({
-				error: true,
-				data: [],
-				message: [err],
-			});
-		} else if (err) {
-			return res.status(500).send({
-				error: true,
-				data: [],
-				message: [err],
-			});
-		}
-
-		next();
-	});
-};
+const { uploadRestoImage } = require('../middlewares/uploadImage');
 
 var loginCtlr = require('../controllers/home/login.controller');
 router.post('/login', loginCtlr.login);
