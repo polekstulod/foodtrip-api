3884cb30a17a35c04867157404c8e37b4c45e83e
paultulod <paultulod@pm.me>
2022-07-06 08:44:29 +0800

    Update upload image middleware - Add updateRestoImage and updateDishImage

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -59,6 +59,68 @@ exports.uploadDishImage = (req, res, next) => {
 	});
 };
 
+exports.updateRestoImage = (req, res, next) => {
+	let restoUpload = multer({
+		storage: restoStorage,
+		fileFilter: helpers.imageFilter,
+	}).single('resto_img');
+
+	restoUpload(req, res, function (err) {
+		if (req.fileValidationError) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [req.fileValidationError],
+			});
+		} else if (err instanceof multer.MulterError) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [err],
+			});
+		} else if (err) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [err],
+			});
+		}
+
+		next();
+	});
+};
+
+exports.updateDishImage = (req, res, next) => {
+	let restoUpload = multer({
+		storage: dishStorage,
+		fileFilter: helpers.imageFilter,
+	}).single('dish_img');
+
+	restoUpload(req, res, function (err) {
+		if (req.fileValidationError) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [req.fileValidationError],
+			});
+		} else if (err instanceof multer.MulterError) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [err],
+			});
+		} else if (err) {
+			return res.status(500).send({
+				error: true,
+				data: [],
+				message: [err],
+			});
+		}
+
+		next();
+	});
+};
+
 exports.uploadRestoImage = (req, res, next) => {
 	let restoUpload = multer({
 		storage: restoStorage,
