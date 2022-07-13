753fe1975eb5436ef383dc60d514bee403f08c64
paultulod <paultulod@pm.me>
2022-07-13 17:29:46 +0800

    Update controller functions - Change from .then to async await

--- src/controllers/resto_admin/dishes.controller.js
+++ src/controllers/resto_admin/dishes.controller.js
@@ -11,40 +11,37 @@ exports.createDish = async (req, res) => {
 	req.body.created_by = req.user.user_id;
 	req.body.dish_no = Math.floor(Math.random() * (99999999999 - 1 + 1) + 1);
 
-	db.Dish.create(req.body)
-		.then((data) => {
-			db.Dish.findByPk(data.dish_id, { include: ['created'] }).then((result) => {
-				dataResponse(res, data, 'Dish has been created successfully', 'Dish was not created');
-			});
-		})
-		.catch((err) => {
-			errResponse(res, err);
-		});
+	try {
+		let data = await db.Dish.create(req.body);
+		let result = await db.Dish.findByPk(data.dish_id, { include: ['created'] });
+		dataResponse(res, result, 'Dish has been created successfully', 'Dish was not created');
+	} catch (err) {
+		errResponse(res, err);
+	}
 };
 
 // * Find all Dish of Restaurant
-exports.getAllDishes = (req, res) => {
+exports.getAllDishes = async (req, res) => {
 	if (!checkAuthorization(req, res, 'Resto_Admin')) {
 		return;
 	}
 
 	const restoId = req.params.restoID;
 
-	db.Dish.findAll({
-		where: { resto_id: restoId },
-		include: ['dish_category', 'restaurant'],
-	})
-		.then((data) => {
-			dataResponse(
-				res,
-				data,
-				'All Dish of this Restaurant has been retrieved',
-				'No Dish of this Restaurant has been retrieved'
-			);
-		})
-		.catch((err) => {
-			errResponse(res, err);
+	try {
+		let data = await db.Dish.findAll({
+			where: { resto_id: restoId },
+			include: ['dish_category', 'restaurant'],
 		});
+		dataResponse(
+			res,
+			data,
+			'All Dish of this Restaurant has been retrieved',
+			'No Dish of this Restaurant has been retrieved'
+		);
+	} catch (err) {
+		errResponse(res, err);
+	}
 };
 
 // * Find single Dish Category of Restaurant
@@ -78,27 +75,26 @@ exports.getDishCat = (req, res) => {
 };
 
 // * Find single Dish of Restaurant
-exports.getDish = (req, res) => {
+exports.getDish = async (req, res) => {
 	if (!checkAuthorization(req, res, 'Resto_Admin')) {
 		return;
 	}
 
 	const id = req.params.dishID;
 
-	db.Dish.findByPk(id, {
-		include: ['dish_category', 'restaurant'],
-	})
-		.then((data) => {
-			dataResponse(
-				res,
-				data,
-				'Dish of this Restaurant has been retrieved',
-				'No Dish of this Restaurant has been retrieved'
-			);
-		})
-		.catch((err) => {
-			errResponse(res, err);
+	try {
+		let data = await db.Dish.findByPk(id, {
+			include: ['dish_category', 'restaurant'],
 		});
+		dataResponse(
+			res,
+			data,
+			'Dish of this Restaurant has been retrieved',
+			'No Dish of this Restaurant has been retrieved'
+		);
+	} catch (err) {
+		errResponse(res, err);
+	}
 };
 
 // * Update Dish
@@ -108,28 +104,24 @@ exports.updateDish = async (req, res) => {
 	}
 
 	const id = req.params.dishID;
-	await db.Dish.findByPk(id, { attributes: ['dish_img'] }).then(
-		(data) => (req.body.dish_img = data.dish_img.slice(-26))
-	);
-	req.body.updated_by = req.user.user_id;
-	if (req.file !== undefined) req.body.dish_img = req.file.filename;
-	console.log(req.body.dish_img);
+	try {
+		let dishImg = await db.Dish.findByPk(id, { attributes: ['dish_img'] });
+		req.body.dish_img = dishImg.dish_img.slice(-26);
+		req.body.updated_by = req.user.user_id;
+		if (req.file !== undefined) req.body.dish_img = req.file.filename;
 
-	db.Dish.update(req.body, {
-		where: { dish_id: id },
-	})
-		.then((result) => {
-			if (result) {
-				db.Dish.findByPk(id, { include: ['updated'] }).then((data) => {
-					dataResponse(res, data, 'Dish has been updated successfully', 'Dish was not updated');
-				});
-			} else {
-				errResponse(res, err);
-			}
-		})
-		.catch((err) => {
-			errResponse(res, err);
+		let result = await db.Dish.update(req.body, {
+			where: { dish_id: id },
 		});
+		if (result) {
+			let data = await db.Dish.findByPk(id, { include: ['updated'] });
+			dataResponse(res, data, 'Dish has been updated successfully', 'Dish was not updated');
+		} else {
+			errResponse(res, err);
+		}
+	} catch (err) {
+		errResponse(res, err);
+	}
 };
 
 // * Delete Dish
@@ -141,27 +133,28 @@ exports.deleteDish = async (req, res) => {
 	const id = req.params.dishID;
 	req.body.deleted_by = req.user.user_id;
 
-	db.Dish.destroy({
-		where: {
-			dish_id: id,
-		},
-	}).catch((err) => {
-		errResponse(res, err);
-	});
+	try {
+		await db.Dish.destroy({
+			where: {
+				dish_id: id,
+			},
+		});
 
-	db.Dish.update(req.body, {
-		where: { dish_id: id },
-		paranoid: false,
-	}).then((result) => {
+		let result = await db.Dish.update(req.body, {
+			where: { dish_id: id },
+			paranoid: false,
+		});
+
 		if (result) {
-			db.Dish.findByPk(id, {
+			let data = await db.Dish.findByPk(id, {
 				paranoid: false,
 				include: ['deleted'],
-			}).then((data) => {
-				dataResponse(res, data, 'Dish has been deleted successfully');
 			});
+			dataResponse(res, data, 'Dish has been deleted successfully');
 		} else {
 			errResponse(res, err);
 		}
-	});
+	} catch (err) {
+		errResponse(res, err);
+	}
 };
