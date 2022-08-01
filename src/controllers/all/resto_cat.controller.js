83fd1b53167991bf495a6b4edd2e9e7cbb6dc680
paultulod <paultulod@pm.me>
2022-08-01 23:56:57 +0800

    Update home restocat controller - Exlude Fast Food from query 😫🤤

--- src/controllers/all/resto_cat.controller.js
+++ src/controllers/all/resto_cat.controller.js
@@ -1,10 +1,14 @@
 const db = require('../../models');
 const { dataResponse, errResponse, emptyDataResponse } = require('../../helpers/controller.helper');
+const { Op } = require('sequelize');
 
 // * Retrieve all Restaurant Category
 exports.getAllRestoCat = async (req, res) => {
 	try {
-		const data = await db.RestoCategory.findAll({ include: 'restaurants' });
+		const data = await db.RestoCategory.findAll({
+			include: 'restaurants',
+			where: { restocatg_name: { [Op.ne]: 'Fast Food' } },
+		});
 		dataResponse(res, data, 'All Restaurant Category has been retrieved', 'No Restaurant Category has been retrieved');
 	} catch (err) {
 		errResponse(res, err);
