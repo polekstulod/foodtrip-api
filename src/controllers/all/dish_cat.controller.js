49a171b6e1c386f12709c55cbae52cf22edd2ab1
paultulod <paultulod@pm.me>
2022-08-01 23:54:45 +0800

    🥺Update home dishcat controller - Limit dishes and dish category response

--- src/controllers/all/dish_cat.controller.js
+++ src/controllers/all/dish_cat.controller.js
@@ -4,7 +4,16 @@ const { dataResponse, errResponse, emptyDataResponse } = require('../../helpers/
 // * Retrieve all Dish
 exports.getAllDishes = async (req, res) => {
 	try {
-		const data = await db.Dish.findAll({ include: ['dish_category', 'restaurant'] });
+		const data = await db.Dish.findAll({
+			include: ['dish_category', 'restaurant'],
+			where: {
+				dishcatg_id: [
+					'b927e32d-30ac-48bf-b272-341f4ed6ff6c',
+					'e45c1775-571d-4804-8f59-c2c456d606f2',
+					'9e4b13a9-ee87-4ab0-a433-df698dc11cfe',
+				],
+			},
+		});
 		dataResponse(res, data, 'All Dishes has been retrieved', 'No Dishes has been retrieved');
 	} catch (err) {
 		errResponse(res, err);
@@ -14,7 +23,10 @@ exports.getAllDishes = async (req, res) => {
 // * Retrieve all Dish Category
 exports.getAllDishCat = async (req, res) => {
 	try {
-		const data = await db.DishCategory.findAll({ include: 'dishes' });
+		const data = await db.DishCategory.findAll({
+			include: 'dishes',
+			where: { dishcatg_name: ['Pasta', 'Rice Dishes', 'Vegetables'] },
+		});
 		dataResponse(res, data, 'All Dish Category has been retrieved', 'No Dish Category has been retrieved');
 	} catch (err) {
 		errResponse(res, err);
