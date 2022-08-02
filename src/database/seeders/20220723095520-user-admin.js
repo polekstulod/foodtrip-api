afb146c87c7c33980cf692bc8d7175045912b5ab
paultulod <paultulod@pm.me>
2022-08-02 16:29:26 +0800

    Create more user_admin seeders

--- src/database/seeders/20220723095520-user-admin.js
+++ src/database/seeders/20220723095520-user-admin.js
@@ -33,6 +33,85 @@ module.exports = {
 				date_created: new Date(),
 				date_updated: new Date(),
 			},
+			/*
+				! Palitan yung
+				TODO first_name 
+				TODO middle_name optional gawin null if blank
+				TODO last_name 
+				TODO email_address 
+				TODO phone_number 
+				TODO gender 
+			 */
+			{
+				user_id: uuidv4(),
+				user_no: `USR-${Math.floor(Date.now() * 1.5) + 20}`,
+				password: await bcrypt.hash('Admin@123', parseInt(process.env.SALT_ROUND)),
+				first_name: 'Micah',
+				middle_name: null,
+				last_name: 'Villaruz',
+				email_address: 'micahmvillaruz@gmail.com',
+				phone_number: '09561437674',
+				gender: 'Female',
+				user_type: 'Admin',
+				date_created: new Date(),
+				date_updated: new Date(),
+			},
+			{
+				user_id: uuidv4(),
+				user_no: `USR-${Math.floor(Date.now() * 1.5) + 21}`,
+				password: await bcrypt.hash('Admin@123', parseInt(process.env.SALT_ROUND)),
+				first_name: 'Micah',
+				middle_name: null,
+				last_name: 'Villaruz',
+				email_address: 'micahmvillaruz@gmail.com',
+				phone_number: '09561437674',
+				gender: 'Female',
+				user_type: 'Admin',
+				date_created: new Date(),
+				date_updated: new Date(),
+			},
+			{
+				user_id: uuidv4(),
+				user_no: `USR-${Math.floor(Date.now() * 1.5) + 22}`,
+				password: await bcrypt.hash('Admin@123', parseInt(process.env.SALT_ROUND)),
+				first_name: 'Micah',
+				middle_name: null,
+				last_name: 'Villaruz',
+				email_address: 'micahmvillaruz@gmail.com',
+				phone_number: '09561437674',
+				gender: 'Female',
+				user_type: 'Admin',
+				date_created: new Date(),
+				date_updated: new Date(),
+			},
+			{
+				user_id: uuidv4(),
+				user_no: `USR-${Math.floor(Date.now() * 1.5) + 23}`,
+				password: await bcrypt.hash('Admin@123', parseInt(process.env.SALT_ROUND)),
+				first_name: 'Micah',
+				middle_name: null,
+				last_name: 'Villaruz',
+				email_address: 'micahmvillaruz@gmail.com',
+				phone_number: '09561437674',
+				gender: 'Female',
+				user_type: 'Admin',
+				date_created: new Date(),
+				date_updated: new Date(),
+			},
+			{
+				user_id: uuidv4(),
+				user_no: `USR-${Math.floor(Date.now() * 1.5) + 24}`,
+				password: await bcrypt.hash('Admin@123', parseInt(process.env.SALT_ROUND)),
+				first_name: 'Micah',
+				middle_name: null,
+				last_name: 'Villaruz',
+				email_address: 'micahmvillaruz@gmail.com',
+				phone_number: '09561437674',
+				gender: 'Female',
+				user_type: 'Admin',
+				date_created: new Date(),
+				date_updated: new Date(),
+			},
 		]);
 	},
 
