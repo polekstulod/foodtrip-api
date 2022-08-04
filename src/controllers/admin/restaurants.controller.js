6c16a485e0e466a4c177e0b18c3d5b14dd4d5ae4
paultulod <paultulod@pm.me>
2022-08-04 14:49:21 +0800

    Update Restaurant controller - getAll resto category - get resto category

--- src/controllers/admin/restaurants.controller.js
+++ src/controllers/admin/restaurants.controller.js
@@ -68,3 +68,62 @@ exports.getRestaurant = async (req, res) => {
 		errResponse(res, err);
 	}
 };
+
+// * Retrieve all Restaurant Category
+exports.getAllRestoCat = async (req, res) => {
+	if (!checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
+	req.body = {
+		draw: 1,
+		columns: [
+			{
+				data: 'restocatg_id',
+				name: '',
+				searchable: true,
+				orderable: true,
+				search: {
+					value: '',
+					regex: false,
+				},
+			},
+		],
+		order: [
+			{
+				column: 0,
+				dir: 'asc',
+			},
+		],
+		start: 0,
+		length: 100,
+		search: {
+			value: '',
+			regex: false,
+		},
+	};
+
+	try {
+		const data = await dataTable(db.RestoCategory, req.body, {
+			include: ['restaurants'],
+		});
+		res.send(data);
+	} catch (err) {
+		errResponse(res, err);
+	}
+};
+
+// * Retrieve single Restaurant Category
+exports.getRestoCat = async (req, res) => {
+	if (!checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
+
+	const id = req.params.restoCatID;
+
+	try {
+		const data = await db.RestoCategory.findByPk(id, { include: ['restaurants'] });
+		dataResponse(res, data, 'Restaurant Category has been retrieved', 'No  Restaurant Category has been retrieved');
+	} catch (err) {
+		errResponse(res, err);
+	}
+};
