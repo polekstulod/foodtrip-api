1b0750ef18661db7a9c43b70e82d3094bd0c2642
paultulod <paultulod@pm.me>
2022-08-05 18:11:39 +0800

    Add admin order controllers - getAllOrders - getOrder

---
+++ src/controllers/admin/orders.controller.js
@@ -0,0 +1,30 @@
+const db = require('../../models');
+const { dataResponse, errResponse, emptyDataResponse, checkAuthorization } = require('../../helpers/controller.helper');
+
+exports.getAllOrders = async (req, res) => {
+	if (!checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
+
+	try {
+		const data = await db.Order.findAll({ include: ['restaurant', 'created'] });
+		dataResponse(res, data, 'All Orders has been retrieved', 'No Orders has been retrieved');
+	} catch (err) {
+		errResponse(res, err);
+	}
+};
+
+exports.getOrder = async (req, res) => {
+	if (!checkAuthorization(req, res, 'Admin')) {
+		return;
+	}
+
+	const id = req.params.orderID;
+
+	try {
+		const data = await db.Order.findOne({ where: { order_id: id }, include: ['address', 'created', 'order_dishes'] });
+		dataResponse(res, data, 'Order has been retrieved', 'No Order has been retrieved');
+	} catch (err) {
+		errResponse(res, err);
+	}
+};
